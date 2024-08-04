1bee0e8b7debec117c4ed382f1c62d31bb02b906
Francisco Oro <francisco.oro@comunidad.unam.mx>
2024-08-03 21:32:58 -0600

    Added checkboxes and radio buttons

--- Chapter09/building-responsive-grid-layouts/src/App.tsx
+++ Chapter09/building-responsive-grid-layouts/src/App.tsx
@@ -1,4 +1,6 @@
 import { Height } from '@mui/icons-material'
+import { Grid, Paper, Typography } from '@mui/material';
+import { Stack, styled } from '@mui/system';
 import React from 'react'
 
 const headerFooterStyle = {
@@ -7,15 +9,48 @@ const headerFooterStyle = {
 };
 const mainStyle = {
   textAlign: "center",
-  height: 50,
+  padding: "8px 16px"
 };
-const Item = {
-  textAlign: "center",
-  height: 50,
-};
+
+const Item = styled(Paper)(() => ({
+  height: "100%",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+})) 
+
 const App = () => {
   return (
-    <div>App</div>
+    <Grid container spacing={2} sx={{ backgroundColor: "#F3F6F9"}}>
+      <Grid xs={12}>
+        <Item sx={headerFooterStyle}>
+          <Typography sx={mainStyle}>Header</Typography>
+        </Item>
+      </Grid>
+
+      <Grid xs="auto">
+        <Item>
+          <Stack spacing={1}>
+            <Typography sx={mainStyle}>Nav Item 1</Typography>
+            <Typography sx={mainStyle}>Nav Item 2</Typography>
+            <Typography sx={mainStyle}>Nav Item 3</Typography>
+            <Typography sx={mainStyle}>Nav Item 4</Typography>
+          </Stack>
+        </Item>
+      </Grid>
+
+      <Grid xs>
+        <Item>
+          <Typography sx={mainStyle}>Main Content</Typography>
+        </Item>
+      </Grid>
+
+      <Grid xs={12}>
+        <Item sx={headerFooterStyle}>
+          <Typography sx={mainStyle}>Footer</Typography>
+        </Item>
+      </Grid>
+    </Grid>
   )
 }
 
--- Chapter09/checkboxes-and-radio-buttons/src/App.tsx
+++ Chapter09/checkboxes-and-radio-buttons/src/App.tsx
@@ -1,34 +1,17 @@
-import React from "react";
-import Checkbox from "@mui/material/Checkbox";
-import Radio from "@mui/material/Radio";
-import RadioGroup from "@mui/material/RadioGroup";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import FormControl from "@mui/material/FormControl";
-import FormLabel from "@mui/material/FormLabel";
+import { FormControlLabel } from "@mui/material"
+import { useState } from "react"
 
-export default function Checkboxes() {
-  const [checkbox, setCheckbox] = React.useState(false);
-  const [radio, setRadio] = React.useState("First");
+const Checkboxes = () => {
+  const [checkbox, setCheckbox] = useState(false);
 
   return (
     <div>
       <FormControlLabel
         label={`Checkbox ${checkbox ? "(checked)" : ""}`}
-        control={
-          <Checkbox
-            checked={checkbox}
-            onChange={() => setCheckbox(!checkbox)}
-          />
-        }
+        
       />
-      <FormControl component="fieldset">
-        <FormLabel component="legend">{radio}</FormLabel>
-        <RadioGroup value={radio} onChange={(e) => setRadio(e.target.value)}>
-          <FormControlLabel value="First" label="First" control={<Radio />} />
-          <FormControlLabel value="Second" label="Second" control={<Radio />} />
-          <FormControlLabel value="Third" label="Third" control={<Radio />} />
-        </RadioGroup>
-      </FormControl>
     </div>
-  );
+  )
 }
+
+export default Checkboxes
