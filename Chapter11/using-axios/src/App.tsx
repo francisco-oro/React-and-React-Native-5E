bfdf89366dd92b4825dc3b6927f7bf837f386468
Francisco Oro <francisco.oro@comunidad.unam.mx>
2024-08-04 20:47:33 -0600

    Added React Query Requests

--- Chapter11/using-axios/src/App.tsx
+++ Chapter11/using-axios/src/App.tsx
@@ -1,28 +1,27 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from "react"
 import { GitHubUser } from "./GitHubUser";
-import UserInfo from "./UserInfo";
 import api from "./api";
+import UserInfo from "./UserInfo";
 
-function App() {
+const App = () => {
   const [user, setUser] = useState<GitHubUser>();
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState<boolean>(true);
 
   useEffect(() => {
     setLoading(true);
 
-    api
-      .getProfile("sakhnyuk")
-      .then((res) => setUser(res.data))
-      .finally(() => setLoading(false));
+    api.getProfile("francisco-oro")
+    .then((res) => setUser(res.data))
+    .finally(() => setLoading(false));  
   }, []);
 
   return (
     <div>
       {loading && <p>Loading...</p>}
-      {!loading && !user && <p>No user found.</p>}
+      {!loading && !user && <p>User not found</p>}
       {user && <UserInfo user={user} />}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- Chapter11/using-axios/src/api.ts
+++ Chapter11/using-axios/src/api.ts
@@ -1,26 +1,26 @@
 import axios, { AxiosInstance } from "axios";
 import { GitHubUser } from "./GitHubUser";
 
-class API {
+class API { 
   private apiInstance: AxiosInstance;
 
-  constructor() {
+  constructor(){ 
     this.apiInstance = axios.create({
       baseURL: "https://api.github.com",
     });
 
     this.apiInstance.interceptors.request.use((config) => {
-      console.log("Request:", `${config.method?.toUpperCase()} ${config.url}`);
+      console.log("Request: ", `${config.method?.toUpperCase()} ${config.url}`)
       return config;
     });
 
     this.apiInstance.interceptors.response.use(
       (response) => {
-        console.log("Response:", response.data);
+        console.log("Response: ", response.data);
         return response;
       },
       (error) => {
-        console.log("Error:", error);
+        console.log("Error: ", error);
         return Promise.reject(error);
       }
     );
@@ -30,5 +30,6 @@ class API {
     return this.apiInstance.get<GitHubUser>(`/users/${username}`);
   }
 }
+
 
 export default new API();
