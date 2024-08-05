bb86f256c7cabdf4ea6ef18edd81fa56ef091475
Francisco Oro <francisco.oro@comunidad.unam.mx>
2024-08-04 19:49:32 -0600

    Managing AsyncUpdates

--- Chapter10/src/AsyncUpdates.tsx
+++ Chapter10/src/AsyncUpdates.tsx
@@ -4,30 +4,28 @@ let unfilteredItems = new Array(25000)
   .fill(null)
   .map((_, i) => ({ id: i, name: `Item ${i}` }));
 
-function filterItems(filter: string): Promise<{ id: number; name: string }[]> {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve(unfilteredItems.filter((item) => item.name.includes(filter)));
-    }, 1000);
-  });
+function filterItems(filter:string) {
+  return new Promise((resolve) => { 
+    setTimeout(() => resolve(unfilteredItems.filter((item) => item.name.includes(filter))), 1000);
+  }) 
 }
 
 export default function AsyncUpdates() {
   const [isLoading, setIsLoading] = React.useState(false);
-  const [filter, setFilter] = React.useState("");
-  const [items, setItems] = React.useState<{ id: number; name: string }[]>([]);
+  let [filter, setFilter] = React.useState("");
+  let [items, setItems] = React.useState<{ id: number; name: string }[]>([]);
 
   const onChange: React.ChangeEventHandler<HTMLInputElement> = async (e) => {
     setFilter(e.target.value);
     setIsLoading(true);
 
     React.startTransition(() => {
-      if (e.target.value === "") {
+      if(e.target.value === "") { 
         setItems([]);
         setIsLoading(false);
-      } else {
+      } else { 
         filterItems(e.target.value).then((result) => {
-          setItems(result);
+          setItems(result as []);
           setIsLoading(false);
         });
       }
@@ -45,7 +43,7 @@ export default function AsyncUpdates() {
         />
       </div>
       <div>
-        {isLoading && <em>loading...</em>}
+        {isLoading && <em>Loading...</em>}
         <ul>
           {items.map((item) => (
             <li key={item.id}>{item.name}</li>
